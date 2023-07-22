75f9538d27c08e408e7087d4eff94b88d031f5c1
sCoruja <strigocoruja@yandex.ru>
2023-07-22 13:37:33 +0300

    Добавлен eslint, typescript

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,23 @@
-import { resolve } from "path";
-import { defineConfig } from "vite";
-import handlebars from "vite-plugin-handlebars";
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
+import handlebars from 'vite-plugin-handlebars';
 
 export default defineConfig({
-    root: resolve(__dirname, 'src'),
-    build:{
-        outDir: resolve(__dirname, 'dist'),
-        rollupOptions:{
-            input:{
-                index: resolve(__dirname, 'src/index.html'),
-                signin: resolve(__dirname, 'src/pages/signin.html'),
-                signup: resolve(__dirname, 'src/pages/signup.html'),
-                error: resolve(__dirname, 'src/pages/error.html'),
-                main: resolve(__dirname, 'src/pages/main.html'),
-                profile: resolve(__dirname, 'src/pages/profile.html')
-            }
-        }
+  root: resolve(__dirname, 'src'),
+  build: {
+    outDir: resolve(__dirname, 'dist'),
+    rollupOptions: {
+      input: {
+        index: resolve(__dirname, 'src/index.html'),
+        signin: resolve(__dirname, 'src/pages/signin.html'),
+        signup: resolve(__dirname, 'src/pages/signup.html'),
+        error: resolve(__dirname, 'src/pages/error.html'),
+        main: resolve(__dirname, 'src/pages/main.html'),
+        profile: resolve(__dirname, 'src/pages/profile.html'),
+      },
     },
-    plugins: [ handlebars()
-    ]
+  },
+  plugins: [handlebars(),
+  ],
 
-}) 
+});
