f6712dd7c669479be3a804d487b7ef3278a35555
sCoruja <strigocoruja@yandex.ru>
2024-09-11 19:26:02 +0300

    gh test fixes

--- mochaSetup.js
+++ mochaSetup.js
@@ -1,8 +1,8 @@
+/* eslint-disable */
 import { JSDOM } from 'jsdom'
 
 const jsdom = new JSDOM('<body></body>', { url: 'https://example.org' })
 
-/* eslint-disable */
 global.window = jsdom.window;
 global.document = jsdom.window.document;
 global.FormData = jsdom.window.FormData;
