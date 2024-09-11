2bb97497498941f1e074911b35f25eba2ba7a69b
sCoruja <strigocoruja@yandex.ru>
2024-09-11 19:18:22 +0300

    gh test fixes

--- mochaSetup.js
+++ mochaSetup.js
@@ -3,6 +3,7 @@ import { JSDOM } from 'jsdom'
 const jsdom = new JSDOM('<body></body>', { url: 'https://example.org' })
 
 
+global.structuredClone = (val) => JSON.parse(JSON.stringify(val))
 global.window = jsdom.window;
 global.document = jsdom.window.document;
 global.FormData = jsdom.window.FormData;
